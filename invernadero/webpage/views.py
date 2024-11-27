8dd67ffa21a8c006eccc0313b41f56b461d2be35
AngelBF5 <101444807+AngelBF5@users.noreply.github.com>
2024-11-27 00:08:28 -0600

    Implementacino de Clima en pagina

--- invernadero/webpage/views.py
+++ invernadero/webpage/views.py
@@ -3,9 +3,41 @@ from .models import Post, PostImage
 from ultralytics import YOLO
 import os
 
+import requests
+from django.shortcuts import render
+from .models import Post  # Asegúrate de que el modelo Post esté importado
+
 def home(request):
+    # Obtener los posts
     posts = Post.objects.prefetch_related("images").all()
-    return render(request, 'home.html', {"posts": posts})
+
+    # Define la clave de API y la URL de la API
+    api_key = 'de7fd6782a3e06e4406140c46f91748d'
+    ciudad = 'Manzanillo,MX'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric&lang=es'
+
+    try:
+        # Realiza la solicitud a la API
+        respuesta = requests.get(url)
+        datos = respuesta.json()
+
+        # Verifica si la respuesta fue exitosa
+        if respuesta.status_code == 200:
+            pronostico = {
+                'ciudad': datos['name'],
+                'temperatura': datos['main']['temp'],
+                'humedad': datos['main']['humidity'],
+                'descripcion': datos['weather'][0]['description'],
+                'icono': datos['weather'][0]['icon'],
+            }
+        else:
+            pronostico = None
+    except Exception as e:
+        pronostico = None
+
+    # Devuelve la vista de inicio con los posts y el pronóstico del clima
+    return render(request, 'home.html', {'posts': posts, 'pronostico': pronostico})
+
 
 
 def detect_objects(request, image_id):
